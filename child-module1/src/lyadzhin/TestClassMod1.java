d38862edf2478a17bacd437c06fb238ea8363b7e
Sergey Lyadzhin <lyadzhin@yandex-team.ru>
2013-11-07 19:00:49 +0400

    test commit

---
+++ child-module1/src/lyadzhin/TestClassMod1.java
@@ -0,0 +1,8 @@
+package lyadzhin;
+
+/**
+ * User: lyadzhin
+ * Date: 07.11.13 18:59
+ */
+public class TestClassMod1 {
+}
---
+++ child-module2/src/lyadzhin/TestClassMod2.java
@@ -0,0 +1,8 @@
+package lyadzhin;
+
+/**
+ * User: lyadzhin
+ * Date: 07.11.13 19:00
+ */
+public class TestClassMod2 {
+}
